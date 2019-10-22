edabc050da979bcef62ee3aa10b7f39b55a63179
Canon Sawrey <csawrey@rightpoint.com>
2019-10-22 10:57:44 -0400

    Serving static files from tut @ realpython.com

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -79,6 +79,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+
 WSGI_APPLICATION = 'personal_website.wsgi.application'
 
 
@@ -136,6 +139,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
